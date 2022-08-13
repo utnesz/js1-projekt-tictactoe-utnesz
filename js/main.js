204d7fcd09e3cf300caa850d0a10d7a8e96dd143
utnesz <90829509+utnesz@users.noreply.github.com>
2022-08-13 20:33:22 +0200

    HTML+CSS almost done, JS stucked

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const matrix =[(cell), (cell), (cell), (cell), (cell), (cell), (cell), (cell), (cell)];
+
+let stepCount = 0;
+
+let cols = (0, 1, 2);
+
+let rows = (0, 1, 2); 
+
+let mark = (mark = 'x');
+
+const initState = [matrix] =  matrix.fill(item => (item = 0));
+console.log(initState);
+
+
+//<p id="demo"></p>
+//
+//<script>
+//const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//document.getElementById("demo").innerHTML = fruits.fill("Kawa");
+//</script>
